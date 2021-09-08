f5394d6db94bc9bd493b0c407c9b123034fbdaba
MZeeshan373 <zeeshanmuhammad94@gmail.com>
2021-09-08 19:48:21 +0500

    changes

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const cors = require('cors');
 //     }
 //   }
 // }
-// app.use(cors(corsOptions));
+app.use(cors());
 
 const authRoutes = require("./routers/auth");
 const userRoutes = require("./routers/user");
@@ -33,16 +33,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Configure Header HTTP
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 
 // Router Basic
 app.use(`/api/${API_VERSION}`, authRoutes);
